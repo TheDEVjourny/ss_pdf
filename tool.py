135a1da2b2afea2186fdf87ce8f4b10404e9043e
Arup <arupsarkarbabu@gmail.com>
2025-06-01 02:02:08 +0530

    tool file is holding the real featurs with bugs

---
+++ tool.py
@@ -0,0 +1,53 @@
+from PIL import ImageGrab
+from functools import partial
+from reportlab.pdfgen import canvas
+import shutil
+
+COUNT = 0
+SS_LIST = []
+END = False
+
+def screen_shot(save_path, main_screen = False):
+    ''' '''
+    ImageGrab.grab = partial(ImageGrab.grab, all_screens=True)
+    BBOX = (1920,0,3850,1080) #(L,T,R,B) # or None 
+    if main_screen:
+        BBOX = None
+    snapshot = ImageGrab.grab(bbox = BBOX)
+    snapshot.save(save_path)
+    snapshot.show()
+    return save_path
+
+def create_notes(images:list):
+    ''' '''
+    fileName = 'Notes.pdf'
+    documentTitle = 'sample'
+    title = 'Findingd'
+    # creating a pdf object
+    pdf = canvas.Canvas(fileName)
+    # setting the title of the document
+    pdf.setTitle(documentTitle)
+    pdf.drawCentredString(300, 770, title)
+    for image in images:
+        pdf.drawInlineImage(image, 0,0)
+        # drawing a line
+        pdf.line(30, 710, 550, 710)
+        shutil.rmtree(image)
+    # saving the pdf
+    print("notes created")
+    pdf.save()
+
+
+
+while(not END):
+    command =  input()
+    if " " in command.lower(): 
+        COUNT += 1
+        image_name = screen_shot(f"ss-{COUNT}.png")
+        SS_LIST.append(image_name)
+    # elif "pdf" in command.lower():
+    #     create_notes(SS_LIST)
+    #     break
+    elif "0" in command.lower():
+        END = True
+        break
